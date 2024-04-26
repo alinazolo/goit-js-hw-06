48fb4dd1acb9053e6856278755aae4c281b65081
Alina <alinazolotopup99@gmail.com>
2024-04-26 20:19:36 +0200

    all 3 tasks update

---
+++ js/task-1.js
@@ -0,0 +1,32 @@
+const customer = {
+  username: "Mango",
+  balance: 24000,
+  discount: 0.1,
+  orders: ["Burger", "Pizza", "Salad"],
+  // Change code below this line
+
+  getBalance() {
+  return this.balance;
+  },
+  getDiscount() {
+   discount = this.discount + 0.05
+    return discount.toFixed(2);
+  },
+  setDiscount(value) {
+    discount = value;
+  },
+  getOrders() {
+  return this.orders;
+  },
+  addOrder(cost, order) {
+    this.balance -= cost - cost * discount;
+    this.orders.push(order);
+  },
+};
+
+
+customer.setDiscount(0.15);
+console.log(customer.getDiscount()); // 0.15
+customer.addOrder(5000, "Steak");
+console.log(customer.getBalance()); // 19750
+console.log(customer.getOrders()); // ["Burger", "Pizza", "Salad", "Steak"]
